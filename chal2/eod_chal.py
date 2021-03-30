19721eacb6238e8f63d028990d7b220aefee915d
Tamarris Jenkins <tamarris.jenkins@gmail.com>
2021-03-30 16:29:35 +0000

    learning about dictionaries

---
+++ chal2/eod_chal.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+##
+heroes=  {
+"wolverine":
+    {"real name": "James Howlett",
+    "powers": "regeneration",
+    "archenemy": "Sabertooth",},
+"harry potter":
+    {"real name": "Harry Potter",
+    "powers": "he's a wizard",
+    "archenemy": "Voldemort",},
+"agent fitz":
+    {"real name": "Leopold Fitz",
+    "powers": "intelligence",
+    "archenemy": "Hydra",}
+        }
+
+char_name = input("Which character do you want to know about? {heroes["wolverine"]},  {heroes["harry potter"]},  {heroes["agent fitz"]}")
+
+char_stat = input ("What statistic do you want to know about?")
+
+##print(char_name + "s" + char_sta
